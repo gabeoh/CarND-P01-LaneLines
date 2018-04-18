bf170c3afd54ab60787167a2fc591ecec8cb0151
gabeoh <jungmoo.oh@gmail.com>
2018-04-18 00:17:54 -0400

    Refactor image lane_detect module to helper package.

---
+++ py-src/helper/lane_detect.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import img_transform as itf
+
+def detect_lane_lines(image):
+    '''
+    Detect lane lines from image using various image transformations
+
+    :param image:
+    :return: original image superimposed with lane lines
+    '''
+
+    # printing out some stats and plotting
+    print('This image is:', type(image), 'with dimensions:', image.shape)
+    ysize = image.shape[0]
+    xsize = image.shape[1]
+
+
+    # First, convert the image into gray scale
+    image_gray = itf.grayscale(image)
+    # plt.imshow(image_gray, cmap='gray')
+
+
+    # Reduce error detection noise by blurring image
+    # Gaussian Blur: kernel_size = 5
+    kernel_size = 5
+    image_blurred = itf.gaussian_blur(image_gray, kernel_size)
+    # plt.imshow(image_blurred, cmap='gray')
+
+
+    # Detect edges
+    # Canny Edge Detection: low_threshold = 50, high_threshold = 150
+    low_threshold = 50
+    high_threshold = 150
+    image_edge = itf.canny(image_blurred, low_threshold, high_threshold)
+    # plt.imshow(image_edge, cmap='gray')
+
+
+    # Find region of interest
+    vertices = np.array([[[120, ysize - 1], [420, 330], [550, 330], [880, ysize - 1]]],
+                        dtype=np.int32)
+    image_regioned = itf.region_of_interest(image_edge, vertices)
+    # plt.imshow(image_regioned, cmap='gray')
+
+
+    # Detect lines using Hough transform
+    rho_accu = 1
+    theta_accu = np.pi / 180
+    hough_threshold = 20
+    min_line_length = 40
+    max_line_gap = 15
+    lines = itf.hough_lines(image_regioned, rho_accu, theta_accu, hough_threshold,
+                            min_line_length, max_line_gap)
+    #plt.imshow(lines)
+
+
+    # Super impose lines on original image
+    image_super = itf.weighted_img(lines, image)
+    #plt.imshow(image_super)
+    return image_super
---
+++ py-src/p01_lanelines_01_image.py
@@ -0,0 +1,23 @@
+#importing some useful packages
+import matplotlib.image as mpimg
+import os
+
+from helper import lane_detect as ld
+
+# Process images under '../test_images/'
+image_src_dir = '../test_images/'
+image_dst_dir = '../test_images_output/'
+images = os.listdir(image_src_dir)
+for image_name in images:
+    print('Reading "%s"...' % image_name)
+    image_path = image_src_dir + image_name
+
+    # Read the image and find lane lines from it
+    image = mpimg.imread(image_path)
+    image_line_detected = ld.detect_lane_lines(image)
+
+    # Write line detected images to files
+    image_out_path = image_dst_dir + image_name
+    mpimg.imsave(image_out_path, image_line_detected)
+    print('The output image is written to: %s\n' % image_out_path)
+
